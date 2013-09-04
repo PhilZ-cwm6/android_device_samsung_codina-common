bb7ec90f09a9c4664799942e2cd06e895a035900
Unjust <android,c357@gmail.com>
2013-09-03 22:14:22 -0500

    Initial Commit for Exhibit variants

---
+++ BoardCommonConfig.mk
@@ -0,0 +1,44 @@
+#
+# Copyright (C) 2013 The Android Open Source Project
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+-include device/samsung/u8500-common/BoardCommonConfig.mk
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1258291200
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1572846592
+BOARD_FLASH_BLOCK_SIZE := 131072
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/samsung/codina-common
+TARGET_USE_ST_ERICSSON_KERNEL := true
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 2048
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/codina-common/bluetooth
+BOARD_BLUEDROID_VENDOR_CONF := device/samsung/codina-common/bluetooth/btvendor_codina.txt
+
+# Vold
+BOARD_VOLD_MAX_PARTITIONS := 25
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
+
+# Recovery
+TARGET_RECOVERY_INITRC := device/samsung/codina-common/recovery/recovery.rc
---
+++ device.mk
@@ -0,0 +1,56 @@
+#
+# Copyright (C) 2013 The Android Open Source Project
+# Copyright (C) 2013 Óliver García Albertos (oliverarafo@gmail.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Inherit common configuration for Samsung u8500 devices
+$(call inherit-product, device/samsung/u8500-common/device_u8500-common.mk)
+
+
+# Recovery
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/prerecovery.rc:root/prerecovery.rc \
+
+# Ramdisk
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/fstab.samsungcodina:root/fstab.samsungcodina \
+    $(LOCAL_PATH)/rootdir/system/lib/modules/j4fs.ko:root/lib/modules/j4fs.ko \
+    $(LOCAL_PATH)/rootdir/system/lib/modules/param.ko:root/lib/modules/param.ko \
+    $(LOCAL_PATH)/rootdir/init.samsungcodina.rc:root/init.samsungcodina.rc \
+    $(LOCAL_PATH)/rootdir/init.samsungcodina.usb.rc:root/init.samsungcodina.usb.rc \
+    $(LOCAL_PATH)/rootdir/ueventd.samsungcodina.rc:root/ueventd.samsungcodina.rc
+
+# TouchScreen & Inputs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/usr/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/configs/usr/keylayout/codina-kp.kl:system/usr/keylayout/codina-kp.kl
+
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    ste.cam.ext.cfg.path.secondary=/system/usr/share/camera/config_file/aptina_mt9v113.dat
+
+# Bluetooth
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/etc/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
+
+# RIL
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/etc/cspsa.conf:system/etc/cspsa.conf
+# Vold
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/etc/vold.fstab:system/etc/vold.fstab
